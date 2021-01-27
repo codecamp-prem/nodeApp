8885b7d8224d8c8f047fde263a97e58303b86109
codecamp-prem <prem.bikram.limbu@gmail.com>
2021-01-27 17:39:53 +0545

    added handlers. Fork writing outdated code. Self Not: delete this repo

--- index.js
+++ index.js
@@ -34,13 +34,59 @@ var server = http.createServer(function(req, res){
   });
   req.on('end', function(){
     buffer += decoder.end();
-    // Send the response
-    res.end("Hello universe");
-    // log the request path
-    console.log('Request Recieved with this payload: ',buffer);
+
+    // choose the handler this request should go to. if not default handler notFound
+    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // construct the data object to send to the handler
+    var data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method' : method,
+      'headers': headers,
+      'payload': buffer
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, function(statusCode, payload){
+      // Use the status code  call backed from handler, default statusCode: 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      // Use  the payload call backed from handler, default payload: {}
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Convert payload to string
+      var payloadString = JSON.stringify(payload);
+
+      // return the response
+      res.setHeader('Content-Type','application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      // log the request path
+      console.log('Returning this response: ',statusCode, payloadString);
+    });
+
   });
 });
 // Start the server, and have it listen on port 3000
 server.listen(3000, function(){
   console.log("listening in port: 3000");
 });
+
+// define the handlers
+var handlers = {};
+
+// Sample Handler
+handlers.sample = function(data,callback){
+  // Callback a http status code, and a payload object
+  callback(406, {'name':'Sample Handler'});
+};
+
+// define a not found handlers
+handlers.notFound = function(data, callback){
+  callback(404);
+};
+
+// define the request router
+var router = {
+  'sample':handlers.sample,
+};
